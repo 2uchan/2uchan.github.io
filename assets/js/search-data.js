f21a41350410265505aee65e39b209b59fecefe1
2uchan <110914689+2uchan@users.noreply.github.com>
2026-03-13 15:54:27 +0000

    Deploying to gh-pages from @ 2uchan/2uchan.github.io@dbd608c6e772f897af77694db5c1598887ad7456 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,12 +30,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+        },{id: "nav-talks",
+          title: "Talks",
+          description: "A list of my talks and presentations.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/talks/";
           },
         },{id: "post-a-post-with-plotly-js",
         
