cab3c7ab655dcfa4e2f96da57f9b62e05ade4e2f
2uchan <110914689+2uchan@users.noreply.github.com>
2026-03-14 02:44:50 +0000

    Deploying to gh-pages from @ 2uchan/2uchan.github.io@5b34095d486e2d733fb318bed377d89bfd1b7479 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,8 +431,8 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
+            },},{id: "projects-lockheed-matin-falcon-challenger",
+          title: 'Lockheed Matin Falcon Challenger',
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
