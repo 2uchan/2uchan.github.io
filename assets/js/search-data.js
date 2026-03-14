95c68aba0b93d6047baff7c701719ac95cdecce3
2uchan <110914689+2uchan@users.noreply.github.com>
2026-03-14 02:34:00 +0000

    Deploying to gh-pages from @ 2uchan/2uchan.github.io@6468ca6960060960eedec5ed6dc2272fc1c51481 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,8 +421,8 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-network-offloading-system-using-data-processing-unit-dpu",
+          title: 'Network Offloading System using Data Processing Unit (DPU)',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
