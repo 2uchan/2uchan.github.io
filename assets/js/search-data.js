f34fe98e4446577efb098352b29a9291547f22b7
2uchan <110914689+2uchan@users.noreply.github.com>
2026-03-14 02:25:28 +0000

    Deploying to gh-pages from @ 2uchan/2uchan.github.io@70b97ca327c0cd02de886cfa84626a416914a9ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,8 +416,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-scalable-python-content-addressable-network-can",
+          title: 'Scalable Python COntent-Addressable Network (CAN)',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
