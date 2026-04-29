db9278f796677bef9e8f3581655975debbd3904c
2uchan <110914689+2uchan@users.noreply.github.com>
2026-04-29 09:23:30 +0000

    Deploying to gh-pages from @ 2uchan/2uchan.github.io@3e3f46d9c7e4ae1fb8d28debbfe996cba53db72d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,6 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-lockheed-martin-falcon-challenger",
-          title: 'Lockheed Martin Falcon Challenger',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-autonomous-driving-club-activity",
-          title: 'Autonomous Driving Club Activity',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
